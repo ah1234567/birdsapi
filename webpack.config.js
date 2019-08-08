48826c54cb61fd9c27262d4dd122c8322d6d8465
Student <jsitbon@qcc.cuny.edu>
2019-08-08 16:33:51 -0400

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-module.exports = {
-  module: {
-    loaders: [
-      { test: /\.css$/, loader: "style-loader!css-loader" },
-      // ...
-    ]
-  }
-};
+// module.exports = {
+//   module: {
+//     loaders: [
+//       { test: /\.css$/, loader: "style-loader!css-loader" },
+//       // ...
+//     ]
+//   }
+// };
